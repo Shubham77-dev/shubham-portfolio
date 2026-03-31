947cf7d5875824c79633bdb2c48ef728b45ff1e4
Shubham Patidar <patidarshubham443@gmail.com>
2026-03-31 15:23:04 +0530

    hide mongo url

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export async function connectDB() {
-  const uri = "mongodb+srv://patidarshubham443_db_user:Shubham1234@cluster0.tchdcls.mongodb.net/?appName=Cluster0"
+  const uri = process.env.MONGODB_URI;
   if (!uri) {
     throw new Error('Missing MONGODB_URI in environment variables');
   }
